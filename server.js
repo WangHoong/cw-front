94f914bf17b47853fb9f4822812750842b0d27ae
houjiazong <houjiazong@gmail.com>
2015-07-09 17:36:58 +0800

    server proxy

--- server.js
+++ server.js
@@ -5,18 +5,46 @@ var route = require('koa-route');
 var path = require('path');
 var config = require('config');
 var logger = require('koa-logger');
+var _ = require('lodash');
+var request = require('co-request');
 
 var app = koa();
 
 var render = views(__dirname + '/views', {
-  map: {html: 'swig'}
+  map: {
+    html: 'swig'
+  }
 });
+
 app.use(logger());
+
 app.use(_static(path.join(__dirname, '/build'), {}));
 
-app.use(route.get('/', function *() {
-  this.body = yield render('index', {API_PREFIX: config['API_PREFIX']});
+if (app.env === 'development') {
+  app.use(function*(next) {
+    if (_.startsWith(this.path, '/api')) {
+      var result = yield request({
+        uri: 'http://dev.api.topdmc.cn' + this.path,
+        method: this.method,
+        headers: {
+          cookie: this.request.headers.cookie
+        },
+        debug: true
+      });
+      this.status = 200;
+      this.body = result.body;
+      return;
+    }
+    yield next;
+  });
+}
+
+app.use(route.get('/', function*() {
+  this.body = yield render('index', {
+    API_PREFIX: config['API_PREFIX']
+  });
 }));
 
-app.listen(9000);
-console.log('listening on port 9000');
+app.listen(9000, function() {
+  console.log('listening on port 9000');
+});
