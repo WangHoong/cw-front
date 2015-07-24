565247d2d4c1bde944eafc129b94d96cc33df0ae
chenyanhong <cyh_lieque@163.com>
2015-07-24 16:16:02 +0800

    修改版权时间

--- app/components/Store/List.jsx
+++ app/components/Store/List.jsx
@@ -30,14 +30,17 @@ var List = React.createClass({
     if(record.composer){
       composer = record.composer;
     }
-    var state = '';
+    var state = '',client_expired='暂无';
     if(record.copyRight.is_exclusive){
       state='已独家';
+      client_expired=record.copyRight.client_expired.substr(0, 10);
     }
     var client = '暂无';
     if(record.copyRight.client){
      client = record.copyRight.client['name'];
     }
+    var startedTime = (record.copyRight.started).substr(0, 10);
+    var expiredTime = (record.copyRight.expired).substr(0, 10);
 
     return(
 
@@ -49,11 +52,11 @@ var List = React.createClass({
             <p className='store-zt'><span className='store-sq'>{state}</span></p>
             <p className='store-name'>{record.name}</p>
             <p className='store-artist'><span>艺人：</span><span>{name}</span></p>
-            <p className='store-yy'><span>作曲：</span>{composer}</p>
-            <p className='store-yy'><span>作词：</span>{lyricist}</p>
+            <p className='store-yy'><span>作曲：</span>{composer}；<span>作词：</span>{lyricist}</p>
             <p className='store-record'><span>所属唱片公司：</span>{record.copyRight.company['name']}</p>
             <p className='store-yy'><span>授权公司：</span>{client}</p>
-            <p className='store-record'><span>版权有效期：</span>2015.3~2020.3</p>
+              <p className='store-yy'><span>授权截止时间：</span>{client_expired}</p>
+            <p className='store-record'><span>版权有效期：</span>{startedTime}~{expiredTime}</p>
             <p className='store-dmc-pri'><span>dmc价：</span><span className='store-price'>￥520.56</span></p>
             <p className='store-add'>加入购物车</p>
           </div>
