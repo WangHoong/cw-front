6ea62606541f8806c0d5366f3e5f9ab4a3da71e3
chenyanhong <cyh_lieque@163.com>
2015-07-24 11:41:46 +0800

    add copyRight

--- app/components/Store/List.jsx
+++ app/components/Store/List.jsx
@@ -18,15 +18,18 @@ var List = React.createClass({
     if(record.lyricist){
       lyricist = record.lyricist;
     }
-    var composer = '未知';
+    var composer = '未知' ;
     if(record.composer){
       composer = record.composer;
     }
-    // if(record.artist)
-    //   record.artist.map(function(item,i){
-    //     console.log(item.name);
-    //   })
-    // }
+    var state = '';
+    if(record.copyRight.is_exclusive){
+      state='已独家';
+    }
+    var client = '暂无';
+    if(record.copyRight.client){
+     client = record.copyRight.client['name'];
+    }
 
     return(
 
@@ -35,13 +38,13 @@ var List = React.createClass({
             <div className='store-img'>
               <img src={imgurl}></img>
             </div>
-            <p className='store-zt'><span className='store-sq'>已独家</span></p>
+            <p className='store-zt'><span className='store-sq'>{state}</span></p>
             <p className='store-name'>{record.name}</p>
             <p className='store-artist'><span>艺人：</span><span>{name}</span></p>
             <p className='store-yy'><span>作曲：</span>{composer}</p>
             <p className='store-yy'><span>作词：</span>{lyricist}</p>
-            <p className='store-record'><span>所属唱片公司：</span>索尼音乐</p>
-            <p className='store-yy'><span>授权公司：</span>QQ音乐</p>
+            <p className='store-record'><span>所属唱片公司：</span>{record.copyRight.company['name']}</p>
+            <p className='store-yy'><span>授权公司：</span>{client}</p>
             <p className='store-record'><span>版权有效期：</span>2015.3~2020.3</p>
             <p className='store-dmc-pri'><span>dmc价：</span><span className='store-price'>￥520.56</span></p>
             <p className='store-add'>加入购物车</p>
