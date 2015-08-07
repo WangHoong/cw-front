f4dc17a7185a08dd77130b5ca2af208cdd6e7556
coogleyao <coogleyao@gmail.com>
2015-08-07 19:04:54 +0800

    add qr

--- app/components/Common/Process.jsx
+++ app/components/Common/Process.jsx
@@ -3,28 +3,35 @@ class Process extends React.Component {
 
   render() {
     let txt
+    let style1 = {marginLeft: '230px', width: '140px'}
     let a =
-    (<div className='card'>
-      <h3 style={{marginTop:0}}>请将下列资料通过微信发送至DMC公众账号：</h3>
-      <ol>
-        <li>独立音乐人本人身份证号</li>
-        <li>身份证正面彩色扫描件或数码照片</li>
-        <li>手持身份证照片</li>
-        <li>本人银行账户（开户行、开户名、账户）</li>
-        <li>联系地址、联系电话</li>
-      </ol>
+    (<div className='card clearfix'>
+      <div style={{float: 'left'}}>
+        <h3 style={{marginTop:0}}>请将下列资料通过微信发送至DMC公众账号：</h3>
+        <ol>
+          <li>1.独立音乐人本人身份证号</li>
+          <li>2.身份证正面彩色扫描件或数码照片</li>
+          <li>3.手持身份证照片</li>
+          <li>4.本人银行账户（开户行、开户名、账户）</li>
+          <li>5.联系地址、联系电话</li>
+        </ol>
+      </div>
+      <img style={style1} src={'../images/topdmc.jpg'} alt="TopDMC微信公众号二维码" />
     </div>)
 
     let b =
-    (<div className='card'>
+    (<div className='card clearfix'>
+    <div style={{float: 'left'}}>
       <h3 style={{marginTop:0}}>请将下列资料通过微信发送至DMC公众账号：</h3>
       <ol>
-        <li>单位营业执照彩色扫描件或数码照</li>
-        <li>组织机构代码证彩色扫描件或数码照片</li>
-        <li>对公银行账户（基本户：开户行、开户名、账户）</li>
-        <li>法定代表人的身份证彩色扫描件或数码照片</li>
-        <li>联系地址、联系电话</li>
+        <li>1.单位营业执照彩色扫描件或数码照</li>
+        <li>2.组织机构代码证彩色扫描件或数码照片</li>
+        <li>3.对公银行账户（基本户：开户行、开户名、账户）</li>
+        <li>4.法定代表人的身份证彩色扫描件或数码照片</li>
+        <li>5.联系地址、联系电话</li>
       </ol>
+      </div>
+      <img style={style1} src={'../images/topdmc.jpg'} alt="TopDMC微信公众号二维码" />
     </div>)
 
     switch (account_type) {
--- app/components/Common/ProcessTips.jsx
+++ app/components/Common/ProcessTips.jsx
@@ -6,18 +6,18 @@ class ProcessTips extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      tip: 0
+      tip: 'hidden'
     }
   }
 
   replaceTip() {
     this.setState({
-      tip: 1
+      tip: 'show'
     })
   }
   render() {
     return (
-      this.state.tip ? (<Process />) : (<a href="javascript:;" onClick={function(){this.replaceTip()}.bind(this)}>欢迎加入DMC！您现在可以查看DMC平台上的歌曲信息。在您完成认证后，第三方服务商才能开始使用您的歌曲，形成收益。</a>)
+      (window.status <= 0) ? ((this.state.tip=='show') ? (<Process />) : (<a href="javascript:;" onClick={function(){this.replaceTip()}.bind(this)}>欢迎加入DMC！您现在可以查看DMC平台上的歌曲信息。在您完成认证后，第三方服务商才能开始使用您的歌曲，形成收益。</a>)) : (<div></div>)
     )
   }
 }
