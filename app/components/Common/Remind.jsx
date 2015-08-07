a1278ad5cb6e1f5078c2e4e99f5d4f5d6731b0f1
chenyanhong <cyh_lieque@163.com>
2015-08-07 14:50:12 +0800

    add mac下载链接

--- app/components/Common/Remind.jsx
+++ app/components/Common/Remind.jsx
@@ -5,8 +5,8 @@ var Remind=React.createClass({
       return (
         <div className='remind'>
           <div className='p1'>温馨提示：请使用Chrome浏览器</div>
-          <a href='http://rj.baidu.com/soft/detail/14744.html?ald'><div className='p2'>点击此处下载</div></a>
-
+          <a href='http://rj.baidu.com/soft/detail/14744.html?ald'><div className='p2'>window系统点击此处下载</div></a>
+          <a href='http://rj.baidu.com/soft/detail/25718.html'><div className='p2'>mac系统点击此处下载</div></a>
         </div>
       )
     }
