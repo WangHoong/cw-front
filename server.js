f74685f7058cc665778b381232e04f3be2f452cc
houjiazong <houjiazong@gmail.com>
2015-08-11 17:53:07 +0800

    remove some images

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var koa = require('koa');
-var views = require('co-views');
 var _static = require('koa-static');
-var route = require('koa-route');
 var path = require('path');
 var config = require('config');
 var logger = require('koa-logger');
-var _ = require('lodash');
-var request = require('co-request');
 var proxy = require('koa-proxy');
 
-var app = koa();
+var router = require('./lib/route');
 
-var render = views(__dirname + '/views', {
-  map: {
-    html: 'swig'
-  }
-});
+var app = koa();
 
 app.use(_static(path.join(__dirname, '/build'), {}));
 
@@ -25,29 +17,10 @@ if (app.env != 'production') {
     host: config['PROXY_PREFIX'],
     match: /(^\/test\/|^\/api\/|^\/online)/
   }));
-  app.use(route.get('/login_demo', function*() {
-    this.body = yield render('login_demo', {
-      API_PREFIX: config['API_PREFIX']
-    });
-  }));
+  app.use(router.dev().routes());
 }
 
-app.use(route.get('/', function*() {
-  this.body = yield render('index', {
-    API_PREFIX: config['API_PREFIX'],
-    LOGIN_URL: config['LOGIN_URL']
-  });
-}));
-
-app.use(route.get('/login', function*() {
-  var url = config['LOGIN_URL'] + '?' + this.querystring;
-  this.redirect(url);
-}));
-
-// 临时
-app.use(route.get('/index', function*() {
-  this.body = yield render('dmc_index');
-}));
+app.use(router.pro().routes());
 
 app.listen(process.env.PORT || 9000, function() {
   console.log('listening on port 9000');
