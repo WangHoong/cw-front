44c56f79d17828c5902739c242a587da2d35f883
houjiazong <houjiazong@gmail.com>
2015-11-12 18:30:18 +0800

    update sp

--- app/components/Main/SP.jsx
+++ app/components/Main/SP.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import AuthorizationHistory from '../SP/AuthorizationHistory.jsx';
 import BillingHistory from '../SP/BillingHistory.jsx';
+import PlayCard from '../SP/PlayCard.jsx';
+import ConsumeCard from '../SP/ConsumeCard.jsx';
 
 class SP extends React.Component {
   render() {
     return (
       <div className='sp-container'>
         <div className='row'>
+          <div className='col-xs-3'>
+            <PlayCard />
+          </div>
+          <div className='col-xs-3'>
+            <ConsumeCard />
+          </div>
+        </div>
+        <div className='row mt10'>
           <div className='col-xs-7'>
             <AuthorizationHistory />
           </div>
