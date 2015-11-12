186c55c3ff6f7a54bcfbecd113b7b7df855df9b7
houjiazong <houjiazong@gmail.com>
2015-11-12 18:30:29 +0800

    update sp

---
+++ app/components/SP/ConsumeCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import TotalCard from '../SP/TotalCard.jsx';
+
+class ConsumeCard extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className='sp-total-card trend-up-background'>
+        <small>当月消费额</small>
+        <h1>&yen; 26900</h1>
+        <div className='row'>
+          <div className='col-xs-6'>
+            <p>已入账</p>
+            <p><strong>2321.80</strong></p>
+          </div>
+          <div className='col-xs-6'>
+            <p>上月消费额</p>
+            <p><strong>23321.80</strong></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ConsumeCard;
