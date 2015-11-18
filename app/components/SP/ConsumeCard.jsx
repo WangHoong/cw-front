a8f1273d0371702c8ac5ec54f6e03d3ba40d35f9
houjiazong <houjiazong@gmail.com>
2015-11-18 17:19:34 +0800

    update sp

--- app/components/SP/ConsumeCard.jsx
+++ app/components/SP/ConsumeCard.jsx
@@ -13,8 +13,8 @@ class ConsumeCard extends React.Component {
         <h1>{numeral(26900).format('$0,0')}</h1>
         <div className='row'>
           <div className='col-xs-6'>
-            <p>已入账</p>
-            <p><strong>{numeral(2321.80).format('0,0')}</strong></p>
+            <p>总消费额</p>
+            <p><strong>{numeral(83761823.80).format('0,0')}</strong></p>
           </div>
           <div className='col-xs-6'>
             <p>上月消费额</p>
--- app/components/SP/PlayCard.jsx
+++ app/components/SP/PlayCard.jsx
@@ -10,15 +10,15 @@ class PlayCard extends React.Component {
     return (
       <div className='sp-total-card trend-down-background'>
         <small>当月消费额</small>
-        <h1>{numeral(26900).format('$0,0')}</h1>
+        <h1>{numeral(93821).format('$0,0')}</h1>
         <div className='row'>
           <div className='col-xs-6'>
-            <p>已入账</p>
-            <p><strong>{numeral(2321.80).format('0,0')}</strong></p>
+            <p>总消费额</p>
+            <p><strong>{numeral(912721312.80).format('0,0')}</strong></p>
           </div>
           <div className='col-xs-6'>
             <p>上月消费额</p>
-            <p><strong>{numeral(23321.80).format('0,0')}</strong></p>
+            <p><strong>{numeral(9812.80).format('0,0')}</strong></p>
           </div>
         </div>
       </div>
