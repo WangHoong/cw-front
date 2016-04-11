ba412af656098e08b5e633fa44bc9c800cbf622c
houjiazong <houjiazong@gmail.com>
2016-04-11 17:11:25 +0800

    dis company type

--- app/components/Common/ProcessTips.jsx
+++ app/components/Common/ProcessTips.jsx
@@ -16,9 +16,36 @@ class ProcessTips extends React.Component {
     })
   }
   render() {
-    return (
-      (window.status <= 0) ? ((this.state.tip=='show') ? (<Process />) : (<a href="javascript:;" onClick={function(){this.replaceTip()}.bind(this)}>{window.lang.Welcome}</a>)) : (<div></div>)
-    )
+    if (window.status <= 0) {
+      if (this.state.tip == 'show') {
+        let company_type = window.currentUser.company_type || 2;
+        if (company_type == 1) {
+          return (
+            <div className='card clearfix'>
+              <div className='pull-left'>
+                <h4>请将下列资料通过微信发送至DMC公众账号：</h4>
+                <ol>
+                  <li>1.音乐人本人身份证号</li>
+                  <li>2.身份证正面彩色扫描件或数码照片</li>
+                  <li>3.本人手持身份证照片</li>
+                  <li>4.本人银行账户（开户行、开户名、账号）</li>
+                  <li>5.联系地址、联系电话</li>
+                </ol>
+              </div>
+              <img style={{marginLeft: '230px', width: '140px'}} src='../images/topdmc.jpg' alt='TopDMC微信公众号二维码' />
+            </div>
+          );
+        } else if (company_type == 2) {
+          return <Process />;
+        } else {
+          return <Process />;
+        }
+      } else {
+        return (
+          <a href='javascript:;' onClick={function() {this.replaceTip()}.bind(this)}>{window.lang.Welcome}</a>
+        );
+      }
+    }
   }
 }
 
