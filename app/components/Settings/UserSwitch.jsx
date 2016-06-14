5c5fcc4ce30d90842df8daa52d77e04dc7a91891
houjiazong <houjiazong@gmail.com>
2016-06-14 14:51:33 +0800

    update switch url

--- app/components/Settings/UserSwitch.jsx
+++ app/components/Settings/UserSwitch.jsx
@@ -8,7 +8,7 @@ var Item = React.createClass({
   },
   render: function(){
     //var url='http://dev.api.topdmc.cn/api/v1/companies/'+this.props.data.id+'/inspect';
-    var url = `${window.DMC_OPT.API_PREFIX}/companies/${this.props.data.id}/inspect`;
+    var url = `${window.DMC_OPT.API_PREFIX}/companies/${this.props.data.id}/inspect?url=${window.location.origin}`;
     return(
       <li className='ce-p'><div className='ce-name'>{this.props.data.name}</div><a href={url}><div className='ce-bt'><button type='button' onClick={this.onClick}>{window.lang.sw}</button></div></a></li>
     )
