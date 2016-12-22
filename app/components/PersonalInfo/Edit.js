75b8b96f8bf5c41400e1fbfe77f132a7e5e7efaa
niux <1021310093@qq.com>
2016-12-22 18:35:01 +0800

    edit-style

--- app/components/PersonalInfo/Edit.js
+++ app/components/PersonalInfo/Edit.js
@@ -70,184 +70,184 @@ var Edit = React.createClass({
     } = this.state
     const representativeName = company_type==1 ? '申请人' : '法人代表'
     const ID_cardName = company_type==1 ? '证件号' : '营业执照号'
-    const pstyles = company_type == '1' ? {backgroundColor: '#98CC6D', width: '80px'} : {width: '80px'}
-    const cstyles = company_type != '1' ? {backgroundColor: '#98CC6D', width: '80px'} : {width: '80px'}
+    const pstyles = company_type == '1' ? {backgroundColor: '#2ed0d7', borderColor: 'transparent' ,color:'#fff', width: '100px'} : {width: '100px'}
+    const cstyles = company_type != '1' ? {backgroundColor: '#2ed0d7', borderColor: 'transparent' ,color:'#fff', width: '100px'} : {width: '100px'}
     const tips = company_type=='1'
       ? '请上传手持身份证正面照，身份证正面照，身份证反面照。'
       : '请上传营业执照照片，法人身份证手持正面照片，法人身份证反面照片。'
     return (
-      <form data-toggle="validator" role="form" id='InfoEdit'>
-        <div className='card'>
-          <h4 style={{display: 'inline'}}>基本资料编辑</h4>
+      <form className='show-wrap' data-toggle="validator" role="form" id='InfoEdit'>
+        <div className='t-sb h61'>
+          <h3 className='t-sb_detail p-l-20'>基本资料编辑</h3>
         </div>
-        <div className='card mt20'>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="account_number" className="col-xs-2 col-form-label text-right" style={{marginTop: '7px'}}>版权方：</label>
-            <div className="col-xs-6">
-              <p className="form-control-static">{window.currentUser.company.name}</p>
-            </div>
-          </div>
-          <div className="form-group row" style={{fontSize: '14px', marginBottom: 0}}>
-            <label for="account_number" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>入驻身份：</label>
-            <div className="col-xs-6">
-              <div className="btn-group" role="group">
-                <button type="button" className="btn btn-default" style={cstyles} onClick={()=>this.toggleCompanyType('2')}>公司</button>
-                <button type="button" className="btn btn-default" style={pstyles} onClick={()=>this.toggleCompanyType('1')}>个人</button>
+        <div className='has-top-bar'>
+          <div className='card margin0 border'>
+            <div className="form-group row ">
+              <label for="account_number" className="col-form-label text-right edit-label" style={{marginTop: '7px'}}>版权方：</label>
+              <div className="textR">
+                <p className="form-control-static">{window.currentUser.company.name}</p>
               </div>
-              <span className="help-block mt10">注：入驻身份确认提交一次后不可再进行修改。</span>
+            </div>
+            <div className="form-group row mb0">
+              <label for="account_number" className="col-form-label text-right edit-label">入驻身份：</label>
+              <div className="textR">
+                <div className="btn-both" role="group">
+                  <button type="button" className="btn btn-default" style={cstyles} onClick={()=>this.toggleCompanyType('2')}>公司</button>
+                  <button type="button" className="btn btn-default" style={pstyles} onClick={()=>this.toggleCompanyType('1')}>个人</button>
+                </div>
+              </div>
+              <div className="text-caution">注：入驻身份确认提交一次后不可再进行修改。</div>
             </div>
           </div>
-        </div>
-        <div className='card mt20'>
-          <div>{company_type==1 ? '负责人基本信息' : '公司基本信息'}</div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="representative" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>{representativeName}：</label>
-            <div className="col-xs-6">
-              <input name='representative' className="form-control" type="text" value={representative} id="representative" onChange={this.handleChange}
-                placeholder={`请输入${representativeName}名称`}
-                data-error='不能为空'
-                required
-              />
+          <div className='card margin0 border mt20 p-b-0'>
+            <div className='deit-title'>{company_type==1 ? '负责人基本信息' : '公司基本信息'}</div>
+            <div className="form-group row">
+              <label for="representative" className="col-form-label text-right edit-label">{representativeName}：</label>
+              <div className="textR">
+                <input name='representative' className="form-control form_control-width" type="text" value={representative} id="representative" onChange={this.handleChange}
+                  placeholder={`请输入${representativeName}名称`}
+                  data-error='不能为空'
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group row" style={{fontSize: '14px'}}>
+              <label for="ID_card" className="col-form-label text-right edit-label" style={{marginTop: '8px'}}>{ID_cardName}：</label>
+              <div className="textR">
+                <input name='ID_card' className="form-control form_control-width" type="text" value={ID_card} id="ID_card" onChange={this.handleChange}
+                  placeholder={`请输入${ID_cardName}`}
+                  data-error='不能为空'
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <label for="phone" className="col-form-label text-right edit-label" style={{marginTop: '8px'}}>联系电话：</label>
+              <div className="textR">
+                <input name='phone' className="form-control form_control-width" type="text" value={phone} id="phone" onChange={this.handleChange}
+                  placeholder='请输入联系电话'
+                  data-error='不能为空'
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <label for="email" className="col-form-label text-right edit-label" style={{marginTop: '8px'}}>常用邮箱：</label>
+              <div className="textR">
+                <input name='email' className="form-control form_control-width" type="email" value={email} id="email" onChange={this.handleChange}
+                  placeholder='请输入常用邮箱'
+                  data-error='请输入正确的邮箱格式'
+                  required
+                />
+                <div className="help-block with-errors"></div>
+              </div>
+            </div>
+            <div className="form-group row">
+              <label for="address" className="col-form-label text-right edit-label" style={{marginTop: '8px'}}>联系地址：</label>
+              <div className="textR">
+                <input name='address' className="form-control form_control-width" type="text" value={address} id="address" onChange={this.handleChange}
+                  placeholder='请输入联系地址'
+                  data-error='不能为空'
+                  required
+                />
+              </div>
             </div>
           </div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="ID_card" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>{ID_cardName}：</label>
-            <div className="col-xs-6">
-              <input name='ID_card' className="form-control" type="text" value={ID_card} id="ID_card" onChange={this.handleChange}
-                placeholder={`请输入${ID_cardName}`}
-                data-error='不能为空'
-                required
-              />
+          <div className='card margin0 border mt20 p-b-0'>
+            <div className='deit-title'>{company_type==1 ? '开户信息' : '公司开户信息'}</div>
+            <div className="form-group row">
+              <label for="bank_name" className="edit-label col-form-label text-right" style={{marginTop: '8px'}}>开户行名称：</label>
+              <div className="textR">
+                <input name='bank_name' className="form-control form_control-width" type="text" value={bank_name} id="bank_name" onChange={this.handleChange}
+                  placeholder='请输入开户行名称'
+                  data-error='不能为空'
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <label for="account_name" className="edit-label col-form-label text-right" style={{marginTop: '8px'}}>开户名称：</label>
+              <div className="textR">
+                <input name='account_name' className="form-control form_control-width" type="text" value={account_name} id="account_name" onChange={this.handleChange}
+                  placeholder={window.currentUser.company.name}
+                  required
+                />
+                <div class="help-block with-errors"></div>
+              </div>
+              <span className="text-caution help-block mt0">注：根据相关法律规定开户名称与版权方必须一致。</span>
+            </div>
+            <div className="form-group row" style={{fontSize: '14px'}}>
+              <label for="account_number" className="edit-label col-form-label text-right" style={{marginTop: '8px'}}>开户行账号：</label>
+              <div className="textR">
+                <input name='account_number' className="form-control form_control-width" type="text" value={account_number} id="account_number" onChange={this.handleChangeAccount}
+                  placeholder='请输入开户行账号'
+                  data-error='不能为空'
+                  required
+                />
+              </div>
             </div>
           </div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="phone" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>联系电话：</label>
-            <div className="col-xs-6">
-              <input name='phone' className="form-control" type="text" value={phone} id="phone" onChange={this.handleChange}
-                placeholder='请输入联系电话'
-                data-error='不能为空'
-                required
-              />
+          <div className='card margin0 border mt20'>
+            <div className='deit-title'>版权认证</div>
+            <div className='row' style={{margin: '10px 30px 10px 60px'}}>
+              <div className='col-md-4'>
+                <UpAvatar
+                  tips={company_type=='1' ? '上传手持身份证正面照' : '上传营业执照照片'}
+                  src={company_type=='1' ? handle_img : license_img}
+                  ref={company_type=='1' ? 'handle_img' : 'license_img'}
+                  type='company_photo'
+                  uploadComplete={() => this.handlePhotoUploaded(`${company_type=='1' ? 'handle_img' : 'license_img'}`)}
+                />
+              </div>
+              <div className='col-md-4'>
+                <UpAvatar
+                  tips={company_type=='1' ? '上传身份证正面照' : '上传法人身份证手持正面照片'}
+                  src={id_card_img}
+                  ref='id_card_img'
+                  type='company_photo'
+                  uploadComplete={() => this.handlePhotoUploaded('id_card_img')}
+                />
+              </div>
+              <div className='col-md-4'>
+                <UpAvatar
+                  tips={company_type=='1' ? '上传身份证反面照' : '上传法人身份证反面照片'}
+                  src={id_card_second_img}
+                  ref='id_card_second_img'
+                  type='company_photo'
+                  uploadComplete={() => this.handlePhotoUploaded('id_card_second_img')}
+                />
+              </div>
             </div>
+            <p className="text-center" style={{color: '#a94442'}}>{this.state.imgErr ? tips : ''}</p>
           </div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="email" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>常用邮箱：</label>
-            <div className="col-xs-6">
-              <input name='email' className="form-control" type="email" value={email} id="email" onChange={this.handleChange}
-                placeholder='请输入常用邮箱'
-                data-error='请输入正确的邮箱格式'
-                required
-              />
-              <div className="help-block with-errors"></div>
-            </div>
-          </div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="address" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>联系地址：</label>
-            <div className="col-xs-6">
-              <input name='address' className="form-control" type="text" value={address} id="address" onChange={this.handleChange}
-                placeholder='请输入联系地址'
-                data-error='不能为空'
-                required
-              />
-            </div>
-          </div>
-        </div>
-        <div className='card mt20'>
-          <div>{company_type==1 ? '开户信息' : '公司开户信息'}</div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="bank_name" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>开户行名称：</label>
-            <div className="col-xs-6">
-              <input name='bank_name' className="form-control" type="text" value={bank_name} id="bank_name" onChange={this.handleChange}
-                placeholder='请输入开户行名称'
-                data-error='不能为空'
-                required
-              />
-            </div>
-          </div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="account_name" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>开户名称：</label>
-            <div className="col-xs-6">
-              <input name='account_name' className="form-control" type="text" value={account_name} id="account_name" onChange={this.handleChange}
-                placeholder={window.currentUser.company.name}
-                required
-              />
-              <span className="help-block mt10" style={{marginBottom: '-8px'}}>注：根据相关法律规定开户名称与版权方必须一致。</span>
-              <div class="help-block with-errors"></div>
-            </div>
-          </div>
-          <div className="form-group row" style={{fontSize: '14px'}}>
-            <label for="account_number" className="col-xs-2 col-form-label text-right" style={{marginTop: '8px'}}>开户行账号：</label>
-            <div className="col-xs-6">
-              <input name='account_number' className="form-control" type="text" value={account_number} id="account_number" onChange={this.handleChangeAccount}
-                placeholder='请输入开户行账号'
-                data-error='不能为空'
-                required
-              />
-            </div>
-          </div>
-        </div>
-        <div className='card mt20'>
-          <div>版权认证</div>
-          <div className='row' style={{margin: '10px 30px 10px 50px'}}>
-            <div className='col-md-4'>
-              <UpAvatar
-                tips={company_type=='1' ? '上传手持身份证正面照' : '上传营业执照照片'}
-                src={company_type=='1' ? handle_img : license_img}
-                ref={company_type=='1' ? 'handle_img' : 'license_img'}
-                type='company_photo'
-                uploadComplete={() => this.handlePhotoUploaded(`${company_type=='1' ? 'handle_img' : 'license_img'}`)}
-              />
-            </div>
-            <div className='col-md-4'>
-              <UpAvatar
-                tips={company_type=='1' ? '上传身份证正面照' : '上传法人身份证手持正面照片'}
-                src={id_card_img}
-                ref='id_card_img'
-                type='company_photo'
-                uploadComplete={() => this.handlePhotoUploaded('id_card_img')}
-              />
-            </div>
-            <div className='col-md-4'>
-              <UpAvatar
-                tips={company_type=='1' ? '上传身份证反面照' : '上传法人身份证反面照片'}
-                src={id_card_second_img}
-                ref='id_card_second_img'
-                type='company_photo'
-                uploadComplete={() => this.handlePhotoUploaded('id_card_second_img')}
-              />
-            </div>
-          </div>
-          <p className="text-center" style={{color: '#a94442'}}>{this.state.imgErr ? tips : ''}</p>
-        </div>
-        <div style={{margin: '10px auto', width: '200px'}}>
-          <button type="submit" className="btn btn-default btn-primary"
-            style={{marginRight: '20px', width: '90px'}}
-            onClick={(e)=>{
-              var info = this.state
-              var that = this
-              $('#InfoEdit').validator().on('submit', function (e) {
-                if (!(that.state.id_card_img && that.state.id_card_second_img && (that.state.handle_img || that.state.license_img))) {
-                  that.state.imgErr = true
-                  that.setState(that.state)
-                }else{
-                  that.state.imgErr = false
-                  that.setState(that.state)
-                }
-                if (e.isDefaultPrevented()) {
-                  // TODO
-                } else {
-                  e.preventDefault()
-                  UserInfoActions.update(that.state)
-                }
-              })
-            }}
-            >提交</button>
-            <button type="button" className="btn btn-default"
-              style={{width: '90px'}}
-              onClick={()=>{
-                this.context.router.transitionTo('base')
+          <div className='text-left mt20'>
+            <button type="submit" className="btn btn-warning mr10 btn-w-h"
+              onClick={(e)=>{
+                var info = this.state
+                var that = this
+                $('#InfoEdit').validator().on('submit', function (e) {
+                  if (!(that.state.id_card_img && that.state.id_card_second_img && (that.state.handle_img || that.state.license_img))) {
+                    that.state.imgErr = true
+                    that.setState(that.state)
+                  }else{
+                    that.state.imgErr = false
+                    that.setState(that.state)
+                  }
+                  if (e.isDefaultPrevented()) {
+                    // TODO
+                  } else {
+                    e.preventDefault()
+                    UserInfoActions.update(that.state)
+                  }
+                })
               }}
-              >取消</button>
+              >提交</button>
+              <button type="button" className="btn btn-default btn-w-h"
+                onClick={()=>{
+                  this.context.router.transitionTo('base')
+                }}
+                >取消</button>
+          </div>
         </div>
       </form>
     )
